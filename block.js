1236602f1a9cfa925c56af2da3cf0b49de1e9240
ProSniper729 <76519132+ProSniper729@users.noreply.github.com>
2021-04-23 00:06:20 +0530

    Update block.js

--- block.js
+++ block.js
@@ -6,9 +6,12 @@ class Block{
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
+        this.Visibility = 255;
+        this.image = loadImage("block.png");
         World.add(world, this.body);
       }
       display(){
+       if(this.body.speed<3){
         var angle = this.body.angle;
         var pos= this.body.position;
         push();
@@ -17,5 +20,15 @@ class Block{
         rectMode(CENTER);
         rect(0,0,this.width, this.height);
         pop();
+        }
+        else{
+          World.remove(world,this.body);
+          push();
+          this.Visibility =  this.Visibility-5;
+          tint(255,this.Visibility);
+          image(this.image,this.body.position.x,this.body.position.y,this.width,this.height)
+          pop();
+            
+        }
       }
 }
